fac313147744eb0bf8328e2b342bd167fbfc85c0
Azimaizat02 <azimaizat02@gmail.com>
2024-01-18 10:33:23 +0800

    remove body admin

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -4,7 +4,7 @@
         <h2 class="h5 no-margin-bottom">Dashboard</h2>
       </div>
     </div>
-    <section class="no-padding-top no-padding-bottom">
+    {{-- <section class="no-padding-top no-padding-bottom">
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-3 col-sm-6">
@@ -375,4 +375,4 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> --}}
